f12d214799711ac4a4fc6577871cd6d0d6bad4e3
guiribeirodev <guiribeirodev@gmail.com>
2026-04-28 18:26:36 -0300

    Refatorando estrutura do projeto: Suporte a asyncio, tornando o projeto não bloqueante

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -13,12 +13,12 @@ app.include_router(auth.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
-def read_root():
+async def read_root():
     return {'message': 'Olá Mundo!'}
 
 
 @app.get('/exercicio-html', response_class=HTMLResponse)
-def hello_world():
+async def hello_world():
     return """
     <html>
       <head>
