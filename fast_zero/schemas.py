eec79567c761cc874920b02d90478852a72464b6
guiribeirodev <guiribeirodev@gmail.com>
2026-04-29 21:43:57 -0300

    Implementado os endpoints de tarefas

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+from fast_zero.models import TodoState
 
 
 class Message(BaseModel):
@@ -30,3 +34,31 @@ class Token(BaseModel):
 class FilterPage(BaseModel):
     offset: int = Field(0, ge=0)
     limit: int = Field(100, ge=1)
+
+
+class TodoSchema(BaseModel):
+    title: str
+    description: str
+    state: TodoState
+
+
+class TodoPublic(TodoSchema):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class TodoList(BaseModel):
+    todos: list[TodoPublic]
+
+
+class FilterTodo(FilterPage):
+    title: str | None = Field(None, min_length=3, max_length=20)
+    description: str | None = Field(None, min_length=3, max_length=20)
+    state: TodoState | None = None
+
+
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    state: TodoState | None = None
--- tests/test_db.py
+++ tests/test_db.py
@@ -3,7 +3,7 @@ from dataclasses import asdict
 import pytest
 from sqlalchemy import select
 
-from fast_zero.models import User
+from fast_zero.models import Todo, TodoState, User
 
 
 @pytest.mark.asyncio
@@ -24,4 +24,50 @@ async def test_create_user(session, mock_db_time):
         'email': 'teste@test',
         'created_at': time,
         'updated_at': time,
+        'todos': [],
     }
+
+
+@pytest.mark.asyncio
+async def test_create_todo(session, user, mock_db_time):
+    with mock_db_time(model=Todo) as time:
+        todo = Todo(
+            title='Test Todo',
+            description='Test Desc',
+            state=TodoState.draft,
+            user_id=user.id,
+        )
+
+        session.add(todo)
+        await session.commit()
+
+    todo = await session.scalar(select(Todo))
+
+    assert asdict(todo) == {
+        'description': 'Test Desc',
+        'id': 1,
+        'state': TodoState.draft,
+        'title': 'Test Todo',
+        'user_id': 1,
+        'created_at': time,
+        'updated_at': time,
+    }
+
+
+@pytest.mark.asyncio
+async def test_user_todo_relationship(session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state=TodoState.draft,
+        user_id=user.id,
+
+    )
+
+    session.add(todo)
+    await session.commit()
+    await session.refresh(user)
+
+    user = await session.scalar(select(User).where(User.id == user.id))
+
+    assert user.todos == [todo]
